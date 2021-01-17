1c00984f3bd3af95337af3153d7449f89dbee9a5
MrUnl <aslanataberk06@gmail.com>
2021-01-17 14:37:41 +0300

    big update

--- assets/script.js
+++ assets/script.js
@@ -22,20 +22,37 @@ if (messageForm != null) {
     })
 }
 
-socket.on('room-created', room => {
+socket.on('room-created', roomInfo => {
+    const {room} = roomInfo
+    console.log(roomInfo);
     const roomElement = document.createElement('li')
+    roomElement.dataset.roomName = room;
     const roomName = document.createElement('span')
+    roomName.classList.add("room-name")
     roomName.innerHTML = room;
+    const userCount = document.createElement('span')
+    userCount.classList.add("room-user-count")
+    userCount.innerHTML = Object.keys(roomInfo.users).length;
     const roomLink = document.createElement('a')
-    roomLink.href = `/${room}`
+    roomLink.href = `/room/${room}`
     roomLink.innerText = 'Katıl'
     roomElement.appendChild(roomName)
+    roomElement.appendChild(userCount)
     roomElement.appendChild(roomLink)
     roomContainer.append(roomElement)
+    document.querySelector(".room-alert").remove()
+})
+socket.on("user-count-update",data => {
+    console.log("aaa");
+    const roomElement = document.querySelector(`[data-room-name=${data.room}]`)
+    console.log(roomElement,data);
+    if(roomElement){
+        roomElement.querySelector(".room-user-count").setAttribute("title",`${data.count} kullanıcı şuan bu odada.`)
+        roomElement.querySelector(".room-user-count").innerHTML = data.count;
+    }
 })
 
 socket.on('chat-message', data => {
-    console.log(data);
     appendMessage(data.message, false, data.name)
 })
 
@@ -43,39 +60,68 @@ socket.on('user-connected', name => {
     appendAlert(`${name} katıldı.`)
 })
 socket.on('room-deleted', rooms => {
-    roomContainer.innerHTML = "";
-    Object.keys(rooms).forEach(room => {
-        const roomElement = document.createElement('li')
-        const roomName = document.createElement('span')
-        roomName.innerHTML = room;
-        const roomLink = document.createElement('a')
-        roomLink.href = `/room/${room}`
-        roomLink.innerText = 'Katıl'
-        roomElement.appendChild(roomName)
-        roomElement.appendChild(roomLink)
-        roomContainer.append(roomElement)
-    })
+    [...roomContainer.querySelectorAll("li")].forEach(el => el.remove())
+    if(Object.keys(rooms).length > 0){
+        Object.keys(rooms).forEach(room => {
+            const roomElement = document.createElement('li')
+            const roomName = document.createElement('span')
+            roomName.innerHTML = room;
+            const roomLink = document.createElement('a')
+            roomLink.href = `/room/${room}`
+            roomLink.innerText = 'Katıl'
+            roomElement.appendChild(roomName)
+            roomElement.appendChild(roomLink)
+            roomContainer.append(roomElement)
+        })
+    }else{
+        const alertMessage = document.createElement("div");
+        alertMessage.classList.add("room-alert")
+        alertMessage.innerHTML = "Herhangi bir oda bulunamadı"
+        roomContainer.append(alertMessage)
+    }
 })
 socket.on('user-disconnected', name => {
     appendAlert(`${name} ayrıldı.`)
 })
 
 function appendAlert(message) {
-    const messageElement = document.createElement('li')
+    const messageElement = document.createElement('div')
+    const alertContainer = document.createElement("li")
     messageElement.classList.add("alert");
+    alertContainer.classList.add("alert-container")
+    alertContainer.appendChild(messageElement)
     messageElement.innerText = message
-    messageContainer.append(messageElement)
+    messageContainer.append(alertContainer)
+    appendSpace()
 }
-
+function appendSpace(){
+    const spaceEl = document.createElement('li');
+    spaceEl.classList.add("space")
+    messageContainer.append(spaceEl)
+}
 function appendMessage(message, you = true, user = null) {
+    const messages = [...messageContainer.children];
+    const lastMessage = messages[messageContainer.childElementCount - 1];
     const messageElement = document.createElement('li')
     if (user !== null) {
         const userElement = document.createElement('span');
         userElement.innerText = user;
-        messageElement.append(userElement)
+        messageElement.classList.add("user")
+        messageElement.dataset.username = user;
+        if(lastMessage.dataset.username && lastMessage.dataset.username === user){
+            messageElement.classList.add("neighbor")
+            userElement.remove()
+        }else{
+            messageElement.append(userElement)
+            appendSpace()
+        }
     }
-    if (you)
+    if (you){
         messageElement.classList.add('you');
+        if(lastMessage.classList.contains('you')){
+            messageElement.classList.add("neighbor")
+        }
+    }
     messageElement.innerHTML += message
     messageContainer.append(messageElement)
 }
--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.post('/room', (req, res) => {
     if (rooms[req.body.room] != null) {
         return res.redirect('/')
     }
-    rooms[req.body.room] = { users: {} }
+    rooms[req.body.room] = { users: {},messages:[] }
     res.redirect("/room/" + req.body.room)
         // Send message that new room was created
-    io.emit('room-created', req.body.room)
+    io.emit('room-created', {room:req.body.room,users:rooms[req.body.room].users})
 })
 
 app.get('/room/:room', (req, res) => {
@@ -38,22 +38,26 @@ io.on('connection', socket => {
     socket.on('new-user', (room, name) => {
         socket.join(room)
         rooms[room].users[socket.id] = name
+        io.emit("user-count-update",{room,count:Object.keys(rooms[room].users).length})
         socket.to(room).broadcast.emit('user-connected', name)
     })
     socket.on('send-chat-message', (room, message) => {
+        console.log(rooms);
+        rooms[room].messages.push({message,name:rooms[room].users[socket.id]})
         socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
     })
     socket.on('disconnect', () => {
         getUserRooms(socket).forEach(room => {
             socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
             delete rooms[room].users[socket.id]
-            console.log(Object.keys(rooms[room].users).length);
+            io.emit("user-count-update",{room,count:Object.keys(rooms[room].users).length})
             if (Object.keys(rooms[room].users).length === 0) {
                 console.log("Room deleted");
                 delete rooms[room];
                 console.log(rooms)
                 io.emit("room-deleted", rooms)
             }
+            
             socket.disconnect();
         })
     })
